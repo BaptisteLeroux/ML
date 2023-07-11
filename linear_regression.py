95f431f4a12d60abf8c1ddc3509fa0f8bd52e234
Baptiste Leroux <baptiste.leroux1999@gmail.com>
2023-07-11 17:57:50 +0200

    Début code linear regression min 7

---
+++ linear_regression.py
@@ -0,0 +1,16 @@
+
+
+class LinearRegression:
+
+    def __init__(self, lr=0.001, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None
+        self.biais = None
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+    
