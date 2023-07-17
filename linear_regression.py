22e2b20c99ed6c8a14dcc7a90d9f682fc7f07ddd
Baptiste Leroux <baptiste.leroux1999@gmail.com>
2023-07-17 17:11:53 +0200

    linear regression algorithms

--- linear_regression.py
+++ linear_regression.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class LinearRegression:
 
@@ -6,11 +6,23 @@ class LinearRegression:
         self.lr = lr
         self.n_iters = n_iters
         self.weights = None
-        self.biais = None
+        self.bias = None
 
     def fit(self, X, y):
-        pass
+        #init parameters
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0 
+
+        for _ in range(self.n_iters):
+            y_predicted = np.dot(X,self.weights) + self.bias
+            dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1/n_samples) * np.sum(y_predicted - y)
+
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+
 
     def predict(self, X):
-        pass
-    
+        y_predicted = np.dot(X, self.weights) + self.bias
+        return y_predicted
